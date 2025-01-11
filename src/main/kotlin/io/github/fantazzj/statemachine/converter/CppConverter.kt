d105e514a6846bdd23998c00333819df2fa02b75
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-01-11 12:40:03 +0100

    writing one files managed by different functions

--- src/main/kotlin/io/github/fantazzj/statemachine/converter/CppConverter.kt
+++ src/main/kotlin/io/github/fantazzj/statemachine/converter/CppConverter.kt
@@ -2,6 +2,7 @@ package io.github.fantazzj.statemachine.converter
 
 import io.github.fantazzj.statemachine.structure.State
 import java.io.File
+import java.io.PrintWriter
 import java.nio.file.Paths
 import kotlin.io.path.createDirectory
 import kotlin.io.path.exists
@@ -21,20 +22,85 @@ class CppConverter(private val name: String, private val states: Collection<Stat
     }
 
     private fun addCppContent(cppFile: File) =
-        cppFile.bufferedWriter().use { out ->
-            out.write("#include \"$name.hpp\"")
+        cppFile.printWriter().use { out ->
+            writeInclude(out)
+            out.println()
+            writeConstructor(out)
+            out.println()
+            writeAutoCycle(out)
+            out.println()
+            writeOutputAnalysis(out)
+            out.println()
+            writeChangeState(out)
+            out.close()
         }
 
+    private fun writeInclude(out: PrintWriter) {
+
+    }
+
+    private fun writeOutputAnalysis(out: PrintWriter) {
+
+    }
+
+    private fun writeConstructor(out: PrintWriter) {
+
+    }
+
+    private fun writeChangeState(out: PrintWriter) {
+
+    }
+
+    private fun writeAutoCycle(out: PrintWriter) {
+
+    }
+
     private fun addHppContent(hppFile: File) =
-        hppFile.bufferedWriter().use { out ->
-            out.write("#ifndef ${name.uppercase()}_HPP\n")
-            out.write("#define ${name.uppercase()}_HPP\n")
-            out.newLine()
-            out.write("class $name {\n")
-            out.newLine()
-            out.write("};\n")
-            out.newLine()
-            out.write("#endif\n")
+        hppFile.printWriter().use { out ->
+            includeGuardsTop(out)
+            out.println()
+            includeFiles(out)
+            out.println()
+            out.println("class $name {")
+            out.println("public:")
+            publicMethods(out)
+            publicAttributes(out)
+            out.println()
+            out.println("private:")
+            privateMethods(out)
+            privateAttributes(out)
+            out.println("};")
+            out.println()
+            includeGuardsBottom(out)
+            out.close()
         }
+
+    private fun includeGuardsTop(out: PrintWriter) {
+        out.println("#ifndef ${name.uppercase()}_HPP")
+        out.println("#define ${name.uppercase()}_HPP")
+    }
+
+    private fun includeFiles(out: PrintWriter) {
+    }
+
+    private fun publicMethods(out: PrintWriter) {
+
+    }
+
+    private fun publicAttributes(out: PrintWriter) {
+
+    }
+
+    private fun privateMethods(out: PrintWriter) {
+
+    }
+
+    private fun privateAttributes(out: PrintWriter) {
+
+    }
+
+    private fun includeGuardsBottom(out: PrintWriter) {
+        out.println("#endif//${name.uppercase()}_HPP")
+    }
 
 }
