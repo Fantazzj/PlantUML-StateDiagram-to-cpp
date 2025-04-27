0670caf7fed48c0f50eabf3b871c42e1699104e8
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-04-27 18:53:52 +0200

    using mavencentral to get plantuml

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation(files("lib/plantuml-1.2025.0.jar"))
+    implementation("net.sourceforge.plantuml:plantuml:1.2025.0")
     implementation("com.github.ajalt.clikt:clikt:5.0.2")
     testImplementation(kotlin("test"))
 }
