68688f321cda99e01a3cc1a32ba1276e53f704ff
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-04-27 19:11:06 +0200

    include into the release also the README and the LICENSE

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,59 @@ application {
 }
 
 runtime {
-    options.addAll("--strip-debug","--no-header-files", "--no-man-pages")
-    modules.addAll("java.base","java.compiler","java.datatransfer","java.desktop","java.instrument","java.logging","java.management","java.management.rmi","java.naming","java.net.http","java.prefs","java.rmi","java.scripting","java.se","java.security.jgss","java.security.sasl","java.smartcardio","java.sql","java.sql.rowset","java.transaction.xa","java.xml","java.xml.crypto","jdk.accessibility","jdk.charsets","jdk.crypto.cryptoki","jdk.crypto.ec","jdk.crypto.mscapi","jdk.dynalink","jdk.httpserver","jdk.incubator.vector","jdk.internal.vm.ci","jdk.internal.vm.compiler","jdk.internal.vm.compiler.management","jdk.jdwp.agent","jdk.jfr","jdk.jsobject","jdk.localedata","jdk.management","jdk.management.agent","jdk.management.jfr","jdk.naming.dns","jdk.naming.rmi","jdk.net","jdk.nio.mapmode","jdk.sctp","jdk.security.auth","jdk.security.jgss","jdk.unsupported","jdk.xml.dom","jdk.zipfs")
+    options.addAll("--strip-debug", "--no-header-files", "--no-man-pages")
+    modules.addAll(
+        "java.base",
+        "java.compiler",
+        "java.datatransfer",
+        "java.desktop",
+        "java.instrument",
+        "java.logging",
+        "java.management",
+        "java.management.rmi",
+        "java.naming",
+        "java.net.http",
+        "java.prefs",
+        "java.rmi",
+        "java.scripting",
+        "java.se",
+        "java.security.jgss",
+        "java.security.sasl",
+        "java.smartcardio",
+        "java.sql",
+        "java.sql.rowset",
+        "java.transaction.xa",
+        "java.xml",
+        "java.xml.crypto",
+        "jdk.accessibility",
+        "jdk.charsets",
+        "jdk.crypto.cryptoki",
+        "jdk.crypto.ec",
+        "jdk.crypto.mscapi",
+        "jdk.dynalink",
+        "jdk.httpserver",
+        "jdk.incubator.vector",
+        "jdk.internal.vm.ci",
+        "jdk.internal.vm.compiler",
+        "jdk.internal.vm.compiler.management",
+        "jdk.jdwp.agent",
+        "jdk.jfr",
+        "jdk.jsobject",
+        "jdk.localedata",
+        "jdk.management",
+        "jdk.management.agent",
+        "jdk.management.jfr",
+        "jdk.naming.dns",
+        "jdk.naming.rmi",
+        "jdk.net",
+        "jdk.nio.mapmode",
+        "jdk.sctp",
+        "jdk.security.auth",
+        "jdk.security.jgss",
+        "jdk.unsupported",
+        "jdk.xml.dom",
+        "jdk.zipfs"
+    )
 }
 
 launch4j {
@@ -58,6 +109,7 @@ tasks.withType<Jar> {
 tasks.register<Zip>("createReleaseGithub") {
     group = "releases"
     dependsOn("createExe", "jre")
+    from("LICENSE", "README.md")
     from("build/launch4j/")
     include("**")
     into("jre") {
