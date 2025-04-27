e0eb6176175fd8489bcb4989681aaa8166fe95eb
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2025-04-27 14:29:16 +0200

    reduced the jdk version to 17 to use the gradle plugin to automatically add the jre to the release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    id("edu.sc.seis.launch4j") version "3.0.6"
     java
     application
+    kotlin("jvm") version "2.0.20"
+    id("edu.sc.seis.launch4j") version "3.0.6"
+    id("org.beryx.runtime") version "1.13.1"
 }
-
-group = "io.github.fantazzj"
-description = rootProject.name
-version = "0.1"
 
 repositories {
     mavenCentral()
@@ -20,19 +17,17 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
 
-tasks.withType<Jar> {
-    manifest {
-        attributes["Main-Class"] = "io.github.fantazzj.MainKt"
-        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") { f -> f.name }
-    }
+application {
+    mainClass = "io.github.fantazzj.MainKt"
+}
+
+runtime {
+    options.addAll("--strip-debug","--no-header-files", "--no-man-pages")
+    modules.addAll("java.base","java.compiler","java.datatransfer","java.desktop","java.instrument","java.logging","java.management","java.management.rmi","java.naming","java.net.http","java.prefs","java.rmi","java.scripting","java.se","java.security.jgss","java.security.sasl","java.smartcardio","java.sql","java.sql.rowset","java.transaction.xa","java.xml","java.xml.crypto","jdk.accessibility","jdk.charsets","jdk.crypto.cryptoki","jdk.crypto.ec","jdk.crypto.mscapi","jdk.dynalink","jdk.httpserver","jdk.incubator.vector","jdk.internal.vm.ci","jdk.internal.vm.compiler","jdk.internal.vm.compiler.management","jdk.jdwp.agent","jdk.jfr","jdk.jsobject","jdk.localedata","jdk.management","jdk.management.agent","jdk.management.jfr","jdk.naming.dns","jdk.naming.rmi","jdk.net","jdk.nio.mapmode","jdk.sctp","jdk.security.auth","jdk.security.jgss","jdk.unsupported","jdk.xml.dom","jdk.zipfs")
 }
 
 launch4j {
@@ -47,4 +42,28 @@ launch4j {
     requiresJdk = false
     bundledJrePath = "jre"
     downloadUrl = ""
+}
+
+group = "io.github.fantazzj"
+description = rootProject.name
+version = "0.1"
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = application.mainClass
+        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") { f -> f.name }
+    }
+}
+
+tasks.register<Zip>("createReleaseGithub") {
+    group = "releases"
+    dependsOn("createExe")
+    from("build/launch4j/")
+    include("**")
+    into("jre") {
+        from("build/jre")
+        include("**")
+    }
+    archiveFileName.set("PlantUML-StateDiagram-to-cpp.zip")
+    destinationDirectory.set(file("build/releases"))
 }
