7c91c7a912edc94627e951ca6231b61f29c7ecd1
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2026-05-12 00:23:13 +0200

    correct getters

--- src/main/kotlin/io/github/fantazzj/statediagram/converter/Converter.kt
+++ src/main/kotlin/io/github/fantazzj/statediagram/converter/Converter.kt
@@ -4,12 +4,10 @@ import io.github.fantazzj.statediagram.structure.Diagram
 
 abstract class Converter(private val diagram: Diagram) {
 
-    private val states = diagram.states
-
     fun getFirstState() = diagram.firstState
 
     fun getName() = diagram.name
 
-    fun getStates() = states
+    fun getStates() = diagram.states
 
 }
